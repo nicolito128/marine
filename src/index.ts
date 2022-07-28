8a704a98174fca1111da2f6f3b37e4a460a99731
Nicolás Serna <nicolito128@hotmail.com>
2022-07-28 15:49:15 -0300

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
+import { urlToBase64 } from '@oasisjs/biscuit/biscuit'; 
 import Client from './client';
 import Message from './../lib/messages/index';
 
 const bot: Client = new Client();
-//const aDay = 24 * 3600 * 1000; // milliseconds in a day
+const A_DAY = 24 * 3600 * 1000; // milliseconds in a day
 const guildId = '973427352560365658';
+const images = [
+    'https://i.imgur.com/fHWZEZK.png',
+    'https://i.imgur.com/nQHBa3I.png',
+    'https://i.imgur.com/5pEGDXx.png',
+    'https://i.imgur.com/ntOpVpP.png',
+    'https://i.imgur.com/tPS4DAg.png',
+    'https://i.imgur.com/HYT7Ouv.png',
+    'https://i.imgur.com/odzUSfQ.png',
+    'https://i.imgur.com/zlCr3No.png',
+    'https://i.imgur.com/agZ4vZW.jpeg'
+];
 
+function selectImage(): () => string {
+    let i: number = 0;
+
+    return function(){
+        i++;
+        if (i == images.length ) {
+            i = 0;
+        }
+
+        return images[i];
+    }
+}
 
 bot.on('ready', ({ user }) => {
     console.log('Logged in as:', user.username);
@@ -21,13 +45,12 @@ bot.on('messageCreate', (msg) => {
     }
 });
 
-bot.on('guildCreate', async (guild) => {
+bot.on('guildCreate', (guild) => {
     if (guild.id === guildId) {
-        guild.edit({
-            iconURL: 'https://i.imgur.com/zlCr3No.png'
-        })
-
-        console.log('Icon edited.');
+        setInterval(async () => {
+            const url = selectImage()();
+            await urlToBase64(url).then(icon => guild.edit({ icon }))
+        }, A_DAY);
     }
 });
 
