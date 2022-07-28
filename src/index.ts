0e3c7f08a7f5c97b2b2062003e998fc55fec5fec
Nicolás Serna <nicolito128@hotmail.com>
2022-07-28 02:19:42 -0300

    Update

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import Client from './client';
 import Message from './../lib/messages/index';
 
 const bot: Client = new Client();
+//const aDay = 24 * 3600 * 1000; // milliseconds in a day
+//const guildId = '973427352560365658';
 
 bot.on('ready', ({ user }) => {
     console.log('Logged in as:', user.username);
@@ -17,6 +19,15 @@ bot.on('messageCreate', (msg) => {
         }
     }
 });
+
+/*
+bot.on('guildCreate', (guild) => {
+    if (guild.id === guildId) {
+        setInterval(() => {
+        }, 2 * 1000)
+    }
+});
+*/
 
 try {
     bot.start();
