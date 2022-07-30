7bf182fcb0d8a7ccb8129a8867c84ba4d40b91cb
Nicolás Serna <nicolito128@hotmail.com>
2022-07-30 01:02:55 -0300

    Fix

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,18 @@ import { LoadEvents, TriggerEvents } from './plugins/plugins';
 client.session.events.on('ready', async ({ user }) => {
     console.log('Logged in as:', user.username);
     console.log('Using prefix: ', client.prefix);
-
-    LoadEvents('guildCreate');
-    LoadEvents('messageCreate');
 });
 
 client.session.events.on('messageCreate', msg => TriggerEvents('messageCreate', msg));
 client.session.events.on('guildCreate', guild => {
     client.guilds.set(guild.id, guild);
+    LoadEvents('guildCreate');
     TriggerEvents('guildCreate', guild);
 });
 
 try {
+    LoadEvents('messageCreate');
+
     client.session.start();
 } catch (err) {
     console.error(err);
