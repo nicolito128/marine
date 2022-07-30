ed2dd91b3143989a0122b025173d5d8746825b4f
Nicolás Serna <nicolito128@hotmail.com>
2022-07-30 16:17:08 -0300

    Docs

--- lib/commands/index.ts
+++ lib/commands/index.ts
@@ -1,6 +1,7 @@
 import CustomMessage from '../../lib/messages';
 import { Client } from '../client';
 
+// Represents essential data for a command.
 export type CommandSchema = {
     name: string;
     description: string;
@@ -14,6 +15,7 @@ export type CommandSchema = {
     cooldown: number;
 };
 
+// Represents the arguments of a command.
 export type ArgumentSchema = {
     name: string;
     description?: string;
@@ -21,14 +23,17 @@ export type ArgumentSchema = {
     type?: string;
 };
 
+// Represents the arguments of a command trigger.
 export type CommandTriggerArgs = {
     message: CustomMessage;
     args: string[];
     client: Client;
 }
 
+// Function to be called when a command is triggered.
 export type CommandTrigger = (args: CommandTriggerArgs) => void | Promise<void>;
 
+// Represents a command common implementation.
 export abstract class CommandStruct implements Partial<CommandSchema> {
     abstract name: string;
     abstract description?: string;
--- lib/messages/index.ts
+++ lib/messages/index.ts
@@ -1,6 +1,10 @@
 import { Message, CreateMessage } from '@biscuitland/core';
 import client, { Client } from '../../src/client';
 
+/**
+ * Creates a custom message object to handle common requirements.
+ * @see {@link Message}
+*/
 export class CustomMessage {
     constructor(message: Message) {
         this.self = message;
@@ -12,18 +16,33 @@ export class CustomMessage {
     readonly client: Client;
     readonly content: string;
 
+    /**
+     * Get the message content without the prefix and the command.
+     */
     get plainContent(): string {
         return this.content.replace(this.client.prefix, '').split(' ').shift() || '';
     }
 
+    /**
+     * If the content starts with the prefix, it returns true.
+     * @param content The content of the message. Optional.
+     */
     hasPrefix(content: string = this.content) {
         return content.startsWith(this.client.prefix);
     }
 
+    /**
+     * Replies a message to the channel.
+     * @param options The options to be used when sending the message.
+     */
     async reply(options: CreateMessage): Promise<CustomMessage> {
         return new CustomMessage(await this.self.reply(options));
     }
 
+    /**
+     * Sends a message to the channel.
+     * @param str The string to be sent.
+     */
     send(str: string) {
         return this.self.reply({ content: str });
     }
