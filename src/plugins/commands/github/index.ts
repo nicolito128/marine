e1958458f32a361cd25dc9509c724912bc5aa9cb
Nicolás Serna <nicolito128@hotmail.com>
2022-08-03 21:59:04 -0300

    Update github command

--- src/plugins/commands/github/index.ts
+++ src/plugins/commands/github/index.ts
@@ -1,3 +1,5 @@
+import { CreateApplicationCommand, Interaction } from '@biscuitland/core';
+import { Client } from '../../../client';
 import type { CommandStruct, CommandTriggerArgs } from '../../../../lib/commands/index';
 
 export const Command = new class implements CommandStruct {
@@ -5,14 +7,23 @@ export const Command = new class implements CommandStruct {
         this.name = 'github';
         this.description = 'Github repository.';
         this.link = 'https://github.com/nicolito128/marine';
+        this.slash = {
+            name: this.name,
+            description: this.description,
+        }
     }
 
     name: string;
     description: string;
+    slash: CreateApplicationCommand;
     link: string;
 
     async trigger({ message }: CommandTriggerArgs) {
         message.send('**Github repository**: ' + this.link)
+    }
+
+    async interaction(_client: Client, int: Interaction) {
+        int.respond({ with: { content: '**Github repository**: ' + this.link } })
     }
 }
 
